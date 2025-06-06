5e9e6ce062fa700fb440ae2308240efe12418994
Andrey Nikiforov <nikiforov@playkot.com>
2025-06-07 00:21:59 +0300

    Вынес открытие поисковика в фикстуру

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selene import browser
 
 @pytest.fixture
 def browser_resolution():
+    browser.config.base_url = 'https://duckduckgo.com/'
     browser.config.window_width = 1280
     browser.config.window_height = 720
     yield
--- search_test.py
+++ search_test.py
@@ -2,12 +2,12 @@ from selene import browser,  have
 
 
 def test_first(browser_resolution):
-    browser.open('https://duckduckgo.com/')
+    browser.open('')
     browser.element('[name="q"]').type('qa.guru').press_enter()
-    browser.element('html').should(have.text('qa.guru — российская онлайн-школа инженеров по автоматизации тестирования'))
+    browser.element('html').should(have.text('Курсы тестировщиков'))
 
 
 def test_second(browser_resolution):
-    browser.open('https://duckduckgo.com/')
+    browser.open('')
     browser.element('[name="q"]').type('авфывфывыфафаыфвфца23123').press_enter()
     browser.element('html').should(have.text('ничего не найдено'))
